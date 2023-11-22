da3ccc7bb8af82e5c7e4df5c756832597b387d16
ogsadmin <ogsadmin@haller-erne.de>
2023-11-22 12:25:31 +0100

    Merged improvements from https://github.com/LuaDist-testing/lua-cjson

--- embarcadero.c
+++ embarcadero.c
@@ -1,5 +1,8 @@
 #include "lua.h"
 
+// CJSON used code from https://github.com/LuaDist-testing/lua-cjson
+// License is MIT
+
 // try to link the correct LUA library
 #if LUA_VERSION_NUM == 501
 #pragma link "lua5.1.lib"
